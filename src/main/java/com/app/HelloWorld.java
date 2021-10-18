acb99fa7693e29b88a5d38c99a784a6f77e04d0d
sadovnichek <kapkaevictor@gmail.com>
2021-10-18 20:40:21 +0500

    add primitive functionality of parsing HTML

--- src/main/java/com/app/HelloWorld.java
+++ src/main/java/com/app/HelloWorld.java
@@ -1,7 +1,27 @@
 package com.app;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HelloWorld {
-    public static void main(String[] argv){
-        System.out.println("Hello World");
+    private static Map<String, String> update() throws IOException {
+        Map<String,String> busStopsNames = new HashMap<>();
+        Document doc = Jsoup.connect("https://www.bustime.ru/ekaterinburg/stop/").get();
+        Elements headLines = doc.getElementsByClass("ui fluid vertical menu")
+                .select("a.item");
+        for (Element headline : headLines) {
+            busStopsNames.put(headline.text(), headline.attr("href"));
+        }
+        return busStopsNames;
+    }
+
+    public static void main(String[] argv) throws IOException {
+        Map<String, String> source = update();
+        System.out.println(source.get("Профессорская"));
     }
 }
